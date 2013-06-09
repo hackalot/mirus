322c73bdb6d36b08033bd5a8290b81171207837b
Joshua Beitler <joshbeitler@gmail.com>
2013-06-09 15:00:04 -0600

    Upgraded keyboard code

--- src/include/hw/keyboard.hpp
+++ src/include/hw/keyboard.hpp
@@ -9,6 +9,32 @@
 #include <stdint.h>
 #include <term/terminal.hpp>
 
+#define CONTROL 0x1
+#define ALT 0x2
+#define ALTGR 0x4
+#define LSHIFT 0x8
+#define RSHIFT 0x10
+#define CAPSLOCK 0x20
+#define SCROLLLOCK 0x40
+#define NUMLOCK 0x80
+
+#define RMASK = 0x80;
+
+typedef uint8_t key_status_t;
+
+typedef struct keymap {
+  // All the chars mapped to their respective scancodes
+  uint8_t scancodes[128];
+  uint8_t shift_scancodes[128];
+
+  // The function keys mapped to the bit position in the key status map.
+  uint8_t control_map[8];
+
+  // The statuses of the control keys, initialized to 0
+  key_status_t controls;
+} keymap_t;
+
+
 namespace mirus {
     void keyboard_handler(struct regs* r);
     void keyboard_install();
--- src/kernel/hw/keyboard.cpp
+++ src/kernel/hw/keyboard.cpp
@@ -1,44 +1,111 @@
 #include <hw/keyboard.hpp>
 
-unsigned char kbdus[128] =
-{
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', /* 9 */
-  '9', '0', '-', '=', '\b', /* Backspace */
-  '\t',         /* Tab */
-  'q', 'w', 'e', 'r',   /* 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\r',     /* Enter key */
-    0,          /* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', /* 39 */
- '\'', '`',   0,        /* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',            /* 49 */
-  'm', ',', '.', '/',   0,                  /* Right shift */
-  '*',
-    0,  /* Alt */
-  ' ',  /* Space bar */
-    0,  /* Caps lock */
-    0,  /* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,  /* < ... F10 */
-    0,  /* 69 - Num lock*/
-    0,  /* Scroll Lock */
-    0,  /* Home key */
-    0,  /* Up Arrow */
-    0,  /* Page Up */
-  '-',
-    0,  /* Left Arrow */
+keymap_t us = {
+  //normal keys
+  {
+    /* first row - indices 0 to 14 */
+    0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+
+    /* second row - indices 15 to 28 */
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', //Enter key
+
+    /* 29 = Control, 30 - 41: third row */
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+
+    /* fourth row, indices 42 to 54, zeroes are shift-keys*/
+    0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0,
+
+    '*',
+
+    /* Special keys */
+
+    0, //ALT - 56
+    ' ', // Space - 57
+    0, //Caps lock - 58
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // F1 to F10 - 59 to 68
+    0, //Num lock - 69
+    0, //Scroll lock - 70
+    0, //Home - 71
+    0, //Up arrow - 72
+    0, //Page up - 73
+    '-',
+    0, //Left arrow - 75
     0,
-    0,  /* Right Arrow */
-  '+',
-    0,  /* 79 - End key*/
-    0,  /* Down Arrow */
-    0,  /* Page Down */
-    0,  /* Insert Key */
-    0,  /* Delete Key */
-    0,   0,   0,
-    0,  /* F11 Key */
-    0,  /* F12 Key */
-    0,  /* All other keys are undefined */
+    0, //Right arrow -77
+    '+',
+    0, //End - 79
+    0, //Dowm arrow - 80
+    0, //Page down - 81
+    0, //Insert - 82
+    0, //Delete - 83
+    0, 0, 0,
+    0, //F11 - 87
+    0, //F12 - 88
+    0, //All others undefined
+  },
+  //caps
+  {
+    /* first row - indices 0 to 14 */
+    0, 27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',
+
+    /* second row - indices 15 to 28 */
+    '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', //Enter key
+
+    /* 29 = Control, 30 - 41: third row */
+    0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',
+
+    /* fourth row, indices 42 to 54, zeroes are shift-keys*/
+    0, '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 0,
+
+    '*',
+
+    /* Special keys */
+
+    0, //ALT - 56
+    ' ', // Space - 57
+    0, //Caps lock - 58
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // F1 to F10 - 59 to 68
+    0, //Num lock - 69
+    0, //Scroll lock - 70
+    0, //Home - 71
+    0, //Up arrow - 72
+    0, //Page up - 73
+    '-',
+    0, //Left arrow - 75
+    0,
+    0, //Right arrow -77
+    '+',
+    0, //End - 79
+    0, //Dowm arrow - 80
+    0, //Page down - 81
+    0, //Insert - 82
+    0, //Delete - 83
+    0, 0, 0,
+    0, //F11 - 87
+    0, //F12 - 88
+    0, //All others undefined
+  },
+
+  // control_map
+  {
+    29, // Ctrl
+    56, // Alt
+    0,  // AltGr
+    42, // left Shift
+    54, // right Shift
+    58, // Caps lock
+    70, // Scroll lock
+    69  // Num lock
+  },
+
+  //Set the initial status of all control keys to "not active"
+  0
 };
+
+keymap_t *current_layout;
+
+uint32_t keyboard_buffer_start, keyboard_buffer_end;
+char keyboard_buffer[256];
 
 void mirus::keyboard_handler(struct regs *r) {
     unsigned char scancode;
@@ -47,7 +114,13 @@ void mirus::keyboard_handler(struct regs *r) {
     if (scancode & 0x80) {
 
     } else {
-        mirus::terminal_putchar(kbdus[scancode]);
+        uint8_t *scancodes = current_layout->scancodes;
+
+        if ((current_layout->controls & (LSHIFT | RSHIFT | CAPSLOCK)) && !(current_layout->controls & CONTROL)) {
+            scancodes = current_layout->shift_scancodes;
+        }
+
+        mirus::terminal_putchar(scancodes[scancode]);
     }
 
     mirus::irq_ack(1);
@@ -56,6 +129,8 @@ void mirus::keyboard_handler(struct regs *r) {
 /* Installs the keyboard handler into IRQ1 */
 void mirus::keyboard_install() {
     mirus::irq_install_handler(1, keyboard_handler);
+    current_layout = &us;
+
     //mirus::irq_ack(1);
 }
 
