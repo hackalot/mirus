ce7434caf61f2545367c3f44e15b0f02349b44aa
Joshua Beitler <joshbeitler@gmail.com>
2013-06-16 16:13:03 -0600

    wut

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -23,8 +23,6 @@ extern "C" void kernel_main()
     // install IRQs
     mirus::irq_install();
 
-    asm volatile("sti");
-
     // initilize display
 	mirus::terminal_initialize();
 
@@ -33,6 +31,8 @@ extern "C" void kernel_main()
 
     // install timer
     mirus::timer_install();
+
+    asm volatile("sti");
 
     // print version number
 	mirus::printf("mirus ");
@@ -46,7 +46,7 @@ extern "C" void kernel_main()
 
     // WHAT THE HELL
     // It seems that the keyboard won't work without this, even if the Timer is not installed.
-    //mirus::timer_wait(1);
+    mirus::timer_wait(1000);
 
     mirus::printf("Write some stuffs: ");
 }
