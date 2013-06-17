1efa638eb9052963ba1176b5824c02c601427e61
Joshua Beitler <joshbeitler@gmail.com>
2013-06-17 15:29:20 -0600

    Fixed issue in the GDT code

--- src/kernel/cpu/gdt.cpp
+++ src/kernel/cpu/gdt.cpp
@@ -28,30 +28,30 @@ namespace mirus {
     }
 }
 
-static mirus::gdt_entry gdt[6];
+static mirus::gdt_entry _gdt[6];
 
 void mirus::gdt::set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran) {
     using namespace mirus;
 
     // descriptor base address
-    gdt[num].base_low = (base & 0xFFFF);
-    gdt[num].base_middle = (base >> 16) & 0xFF;
-    gdt[num].base_high = (base >> 24) & 0xFF;
+    _gdt[num].base_low = (base & 0xFFFF);
+    _gdt[num].base_middle = (base >> 16) & 0xFF;
+    _gdt[num].base_high = (base >> 24) & 0xFF;
 
     // limits
-    gdt[num].limit_low = (limit & 0xFFFF);
-    gdt[num].granularity = ((limit >> 16) & 0x0F);
+    _gdt[num].limit_low = (limit & 0xFFFF);
+    _gdt[num].granularity = ((limit >> 16) & 0x0F);
 
     // flags + granularity
-    gdt[num].granularity |= (gran & 0xF0);
-    gdt[num].access = access;
+    _gdt[num].granularity |= (gran & 0xF0);
+    _gdt[num].access = access;
 }
 
 void mirus::gdt::install() {
     using namespace mirus;
 
     gp.limit = (sizeof(gdt_entry) * 6) - 1;
-    gp.base = (unsigned long)&gdt;
+    gp.base = (unsigned long)&_gdt;
 
     // Null descriptor
     gdt::set_gate(0, 0, 0, 0, 0);
