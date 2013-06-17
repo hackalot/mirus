5b76a217bec88c1ab197e22102ba9c58f040a6df
Joshua Beitler <joshbeitler@gmail.com>
2013-06-17 15:32:13 -0600

    Cleaned up IDT code

--- src/include/cpu/gdt.hpp
+++ src/include/cpu/gdt.hpp
@@ -47,30 +47,4 @@ namespace mirus {
     };
 }
 
-
-// namespace mirus {
-//     // entry in the gdt
-//     class gdt_entry {
-//     public:
-//         unsigned short limit_low;
-//         unsigned short base_low;
-//         unsigned char base_middle;
-//         unsigned char access;
-//         unsigned char granularity;
-//         unsigned char base_high;
-//     } __attribute__ ((__packed__));
-
-//     // ptr to the gdt
-//     class gdt_ptr {
-//     public:
-//         unsigned short limit;
-//         unsigned long base;
-//     } __attribute__ ((__packed__));
-
-//     void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
-
-//     // install the gdt
-//     void gdt_install();
-// }
-
 #endif
--- src/include/cpu/idt.hpp
+++ src/include/cpu/idt.hpp
@@ -24,7 +24,6 @@
 #include <mem/memory.hpp>
 
 namespace mirus {
-    /* Defines an IDT entry */
     class idt_entry {
     public:
         unsigned short base_lo;
@@ -40,8 +39,39 @@ namespace mirus {
         unsigned long base;
     } __attribute__ ((__packed__));
 
-    void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
-    void idt_install();
+    class idt {
+    public:
+        static void set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+        static void install();
+    };
 }
+
+
+
+
+
+
+
+
+// namespace mirus {
+//     /* Defines an IDT entry */
+//     class idt_entry {
+//     public:
+//         unsigned short base_lo;
+//         unsigned short sel;        /* Our kernel segment goes here! */
+//         unsigned char always0;     /* This will ALWAYS be set to 0! */
+//         unsigned char flags;       /* Set using the above table! */
+//         unsigned short base_hi;
+//     } __attribute__ ((__packed__));
+
+//     class idt_ptr {
+//     public:
+//         unsigned short limit;
+//         unsigned long base;
+//     } __attribute__ ((__packed__));
+
+//     void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+//     void idt_install();
+// }
 
 #endif
