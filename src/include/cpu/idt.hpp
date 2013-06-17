77ff5d64815d51943fc14d1652272b65fb392a93
Joshua Beitler <joshbeitler@gmail.com>
2013-06-17 15:36:06 -0600

    Updated IDT

--- src/include/cpu/idt.hpp
+++ src/include/cpu/idt.hpp
@@ -27,9 +27,9 @@ namespace mirus {
     class idt_entry {
     public:
         unsigned short base_lo;
-        unsigned short sel;        /* Our kernel segment goes here! */
-        unsigned char always0;     /* This will ALWAYS be set to 0! */
-        unsigned char flags;       /* Set using the above table! */
+        unsigned short sel;        // Our kernel segment goes here!
+        unsigned char always0;     // This will ALWAYS be set to 0!
+        unsigned char flags;       // Set using the above table!
         unsigned short base_hi;
     } __attribute__ ((__packed__));
 
@@ -45,33 +45,5 @@ namespace mirus {
         static void install();
     };
 }
-
-
-
-
-
-
-
-
-// namespace mirus {
-//     /* Defines an IDT entry */
-//     class idt_entry {
-//     public:
-//         unsigned short base_lo;
-//         unsigned short sel;        /* Our kernel segment goes here! */
-//         unsigned char always0;     /* This will ALWAYS be set to 0! */
-//         unsigned char flags;       /* Set using the above table! */
-//         unsigned short base_hi;
-//     } __attribute__ ((__packed__));
-
-//     class idt_ptr {
-//     public:
-//         unsigned short limit;
-//         unsigned long base;
-//     } __attribute__ ((__packed__));
-
-//     void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
-//     void idt_install();
-// }
 
 #endif
