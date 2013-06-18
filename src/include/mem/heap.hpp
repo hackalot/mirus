a6623cc3d82be1e60d4913eaa36ecd78d253e6b8
Joshua Beitler <joshbeitler@gmail.com>
2013-06-18 17:02:11 -0600

    Pull that crap out

--- src/include/mem/heap.hpp
+++ src/include/mem/heap.hpp
@@ -20,74 +20,9 @@
 
 #include <stddef.h>
 #include <stdint.h>
-
-#define IS_PAGE_ALIGNED(x) ((  ((uint32_t)(x)) & 0x00000fff) == 0)
-#define IS_DWORD_ALIGNED(x) (( ((uint32_t)(x)) & 3) == 0)
-
-#define HEAP_MAGIC 0xabcdef12
-
-#define AREA_USED 0
-#define AREA_FREE 1
-
-#define FOOTER_FROM_HEADER(__h) ((area_footer_t *)((uint32_t)__h + __h->size - sizeof(area_footer_t)))
-
-#define HEAP_INDEX_SIZE 0x10000
-
-#define KHEAP_START 0xc0000000
-#define KHEAP_MAX_ADDR 0xcffff000 /* one page less than 0xd000000 */
-
-/* Values for heap debugging */
-#define KHEAP_INITIAL_SIZE 0x90000 /* 64 kiB + 512k for the indexes */
-#define KHEAP_MIN_GROWTH 0x8000 /* 32 kiB */
-#define KHEAP_MAX_WASTE 0x120000 /* must be >1 MiB */
-
-#define USER_HEAP_INITIAL_SIZE 0x90000
-#define USER_HEAP_MIN_GROWTH 0x8000
-#define USER_HEAP_MAX_WASTE 0x120000
-
-#define USER_HEAP_START 0x20000000
-#define USER_HEAP_MAX_ADDR 0xbff00000
+#include <util/string.hpp>
 
 namespace mirus {
-    typedef struct {
-        uint32_t size; /* includes the header and footer! */
-        uint8_t type; /* == AREA_USED (0) || AREA_FREE (1) */
-        uint32_t magic;
-    } area_header_t;
-
-    typedef struct {
-        uint32_t magic;
-        area_header_t *header;
-    } area_footer_t;
-
-    typedef struct {
-        uint32_t start_address;
-        uint32_t end_address;
-        uint32_t _min_address; // start address is for the storage; _min_address for the entire heap w/ indexes etc.
-        uint32_t max_address;
-        uint8_t supervisor;
-        uint8_t readonly;
-
-        ordered_array_t free_index; /* Stores an array of area_header_t pointers */
-        ordered_array_t used_index; /* Stores an array of area_header_t pointers */
-
-        area_header_t *rightmost_area; /* A pointer to the rightmost area, free or used. Used in both alloc() and free(). */
-
-        mutex_t *mutex;
-    } heap_t;
-
-    void stop_leak_trace(void);
-    void start_leak_trace(void);
-
-    heap_t *heap_create(uint32_t start_address, uint32_t initial_size, uint32_t max_address, uint8_t supervisor, uint8_t readonly, struct task_mm *mm);
-    void heap_destroy(heap_t *heap, page_directory_t *dir);
-    void *heap_alloc(uint32_t size, bool page_align, heap_t *heap);
-    void heap_free(void *p, heap_t *heap);
-
-    void validate_heap_index(bool print_headers);
-    void print_heap_index(void);
-
-
     void kfree(void* p);
 
     void* malloc(size_t);
