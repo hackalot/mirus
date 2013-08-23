fe20250ceaadf01bc2460523771b2a629b40fe39
Joshua Beitler <joshbeitler@gmail.com>
2013-08-23 17:59:48 -0600

    Changed terminal_ to terminal - removed the old ones

--- source/include/term/terminal.hpp
+++ source/include/term/terminal.hpp
@@ -12,77 +12,67 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef _MIRUS_KERNEL_TERMINAL_H_
-#define _MIRUS_KERNEL_TERMINAL_H_
+#ifndef _MIRUS_KERNEL_IO_TERM_H_
+#define _MIRUS_KERNEL_IO_TERM_H_
 
-#include <stddef.h>
-#include <stdint.h>
-#include <util/string.hpp>
-#include <core/ioport.hpp>
-#include <mem/memory.hpp>
-
-// TODO: move under mirus namespace
-// TODO: move under a mirus::io::term class (or similar)
-// TODO: prettyfy/make simpiler
-
-// Hardware text mode color constants.
-enum vga_color
-{
-    COLOR_BLACK = 0,
-    COLOR_BLUE = 1,
-    COLOR_GREEN = 2,
-    COLOR_CYAN = 3,
-    COLOR_RED = 4,
-    COLOR_MAGENTA = 5,
-    COLOR_BROWN = 6,
-    COLOR_LIGHT_GREY = 7,
-    COLOR_DARK_GREY = 8,
-    COLOR_LIGHT_BLUE = 9,
-    COLOR_LIGHT_GREEN = 10,
-    COLOR_LIGHT_CYAN = 11,
-    COLOR_LIGHT_RED = 12,
-    COLOR_LIGHT_MAGENTA = 13,
-    COLOR_LIGHT_BROWN = 14,
-    COLOR_WHITE = 15,
-};
+#include <system.hpp>
 
 namespace mirus
 {
-    // create a color
-    uint8_t make_color(enum vga_color fg, enum vga_color bg);
+    namespace io
+    {
+        typedef uint8_t terminal_color;
+        
+        class terminal
+        {
+        public:
+            enum class vga_color
+            {
+                black         = 0,
+                blue          = 1,
+                green         = 2,
+                cyan          = 3,
+                red           = 4,
+                magenta       = 5,
+                brown         = 6,
+                light_grey    = 7,
+                dark_grey     = 8,
+                light_blue    = 9,
+                light_green   = 10,
+                light_cyan    = 11,
+                light_red     = 12,
+                light_magenta = 13,
+                light_brown   = 14,
+                white         = 15
+            };
+           
+            // place a character at a specified location
+            static void putentry(char c, terminal_color color, size_t x, size_t y);
 
-    // create a VGA entry
-    uint16_t make_vgaentry(char c, uint8_t color);
+            // place a character
+            static void putchar(char c);
+            static void putchar(char c, terminal_color color);
 
-    // init the terminal
-    void terminal_initialize();
+            // place a string
+            static void writestring(const char* data);
+            static void writestring(const char* data, terminal_color color);
 
-    // set color
-    void terminal_setcolor(uint8_t color);
+            // clear the screen
+            static void clear();
+            static void clear(terminal_color color);
 
-    // put an entry at location
-    void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
+            static void scroll();
+            static void move_cursor();
 
-    // put a char at a location
-    void terminal_putchar(char c);
-    void terminal_putchar(char c, uint8_t color);
+            // make a color/entry for vga memory
+            static terminal_color make_color(vga_color fg, vga_color bg);
+            static uint16_t make_vga_entry(char c, terminal_color color);
 
-    // write a string
-    void terminal_writestring(const char *data);
-    void terminal_writestring(const char *data, uint8_t color);
+            static void set_color(terminal_color color);
 
-    // clear function
-    void terminal_clear();
-    void terminal_clear(uint8_t color);
-
-    // our test void
-    void test();
-
-    // scroll it
-    void terminal_scroll();
-
-    // move cursor
-    void terminal_move_cursor();
+            static void initilize();
+        };
+    }
 }
 
 #endif
--- source/kernel/term/terminal.cpp
+++ source/kernel/term/terminal.cpp
@@ -12,234 +12,164 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <term/terminal.hpp>
+#include <term/terminal_.hpp>
 
-// terminal sizes
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 24;
-
-// term info
-static size_t terminal_row;
-static size_t terminal_column;
-static uint8_t terminal_color;
-static uint16_t* terminal_buffer;
-
-uint8_t mirus::make_color(enum vga_color fg, enum vga_color bg)
+namespace mirus
 {
-    return fg | bg << 4;
-}
-
-uint16_t mirus::make_vgaentry(char c, uint8_t color)
-{
-    uint16_t c16 = c;
-    uint16_t color16 = color;
-
-    return c16 | color16 << 8;
-}
-
-void mirus::terminal_initialize()
-{
-    using namespace mirus;
-
-    terminal_row = 0;
-    terminal_column = 0;
-    terminal_color = make_color(COLOR_WHITE, COLOR_BLACK);
-    terminal_buffer = (uint16_t*) 0xB8000;
-
-    for ( size_t y = 0; y < VGA_HEIGHT; y++ )
+    namespace io
     {
-        for ( size_t x = 0; x < VGA_WIDTH; x++ )
+        // terminal sizes
+        const size_t VGA_WIDTH = 80;
+        const size_t VGA_HEIGHT = 24;
+
+        // term info
+        size_t terminal_row;
+        size_t terminal_column;
+        uint8_t _terminal_color;
+        uint16_t* terminal_buffer;
+
+        void terminal::putentry(char c, terminal_color color, size_t x, size_t y)
         {
             const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+            terminal_buffer[index] = terminal::make_vga_entry(c, color);
         }
-    }
-}
 
-// set color
-void mirus::terminal_setcolor(uint8_t color)
-{
-    terminal_color = color;
-}
-
-// put an entry at location
-void mirus::terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
-{
-    using namespace mirus;
-
-    const size_t index = y * VGA_WIDTH + x;
-    terminal_buffer[index] = make_vgaentry(c, color);
-}
-
-// put a char at a location
-void mirus::terminal_putchar(char c)
-{
-    using namespace mirus;
-
-    if (c == '\r')
-    {
-        terminal_row++;
-        terminal_column = 0;
-    }
-    else if (c == '\b')
-    {
-        terminal_column--;
-        terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
-    }
-    else
-    {
-        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-
-        if ( ++terminal_column == VGA_WIDTH )
+        void terminal::putchar(char c)
         {
+            if (c == '\r')
+            {
+                terminal_row++;
+                _terminal_color = 0;
+            }
+            else if (c == '\b')
+            {
+                _terminal_color--;
+                terminal::putentry(' ', _terminal_color, terminal_column, terminal_row);
+            }
+            else
+            {
+                terminal::putentry(c, _terminal_color, terminal_column, terminal_row);
+            }
+
+            terminal::move_cursor();
+        }
+
+        void terminal::putchar(char c, terminal_color color)
+        {
+            terminal_color oldcolor = _terminal_color;
+
+            terminal::set_color(color);
+            terminal::putchar(c);
+            terminal::set_color(oldcolor);
+        }
+
+        void terminal::writestring(const char* data)
+        {
+            size_t datalen = strlen(data);
+
+            for (size_t i = 0; i < datalen; i++)
+            {
+                // TODO: do we really need this?
+                // if (data[i] == '\r')
+                // {
+                //     terminal_row++;
+                //     terminal_column = 0;
+                // }
+                // else if (data[i] == '\b')
+                // {
+                //     terminal_column--;
+                //     terminal::putentry(' ', _terminal_color, terminal_column, terminal_row);
+                // }
+                // else
+                // {
+                //     terminal::putchar(data[i]);
+                // }
+                terminal::putchar(data[i]);
+            }
+        }
+
+        void terminal::writestring(const char* data, terminal_color color)
+        {
+            terminal_color prev_color = _terminal_color;
+            terminal::set_color(color);
+
+            terminal::writestring(data);
+
+            terminal::set_color(prev_color);
+        }
+
+        void terminal::clear()
+        {
+            for (int i = 0; i < 80 * 25; i++)
+            {
+                terminal_buffer[i] = 0;
+            }
+
+            // Move the hardware cursor back to the start.
+            terminal_row = 0;
             terminal_column = 0;
-            terminal_row++;
 
-            // if (++terminal_row == 25)
-            // {
-            //     terminal_scroll();
-            // }
-
+            terminal::move_cursor();
         }
-    }
 
-    // TODO: bug here
-    // if (++terminal_row == VGA_HEIGHT) {
-    //     terminal_scroll();
-    // }
-
-    terminal_move_cursor();
-}
-
-void mirus::terminal_putchar(char c, uint8_t color)
-{
-    uint8_t oldcolor = terminal_color;
-    terminal_setcolor(color);
-    mirus::terminal_putchar(c);
-    terminal_setcolor(oldcolor);
-}
-
-// write a string
-void mirus::terminal_writestring(const char* data)
-{
-    using namespace mirus;
-
-    size_t datalen = strlen(data);
-
-    for (size_t i = 0; i < datalen; i++)
-    {
-        if (data[i] == '\r')
+        void terminal::clear(terminal_color color)
         {
-            terminal_row++;
+             for (int i = 0; i < 80 * 25; i++)
+            {
+                terminal::putchar(' ', color);
+            }
+
+            // Move the hardware cursor back to the start.
+            terminal_row = 0;
             terminal_column = 0;
-        }
-        else if (data[i] == '\b')
-        {
-            terminal_column--;
-            terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
-        }
-        else
-        {
-            terminal_putchar(data[i]);
-        }
-    }
-}
 
-void mirus::terminal_writestring(const char* data, uint8_t color)
-{
-    using namespace mirus;
+            terminal::move_cursor();
+        }
 
-    size_t datalen = strlen(data);
-    for (size_t i = 0; i < datalen; i++)
-    {
-        if (data[i] == '\r')
+        void terminal::scroll()
         {
-            terminal_row++;
+
+        }
+
+        void terminal::move_cursor()
+        {
+            unsigned temp;
+
+            // Index = [(y * width) + x]
+            temp = terminal_row * 80 + terminal_column;
+
+            mirus::outb(0x3D4, 14);
+            mirus::outb(0x3D5, temp >> 8);
+            mirus::outb(0x3D4, 15);
+            mirus::outb(0x3D5, temp);
+        }
+
+        terminal_color terminal::make_color(vga_color fg, vga_color bg)
+        {
+            return (int)fg | (int)bg << 4;
+        }
+
+        uint16_t terminal::make_vga_entry(char c, terminal_color color)
+        {
+            uint16_t c16 = c;
+            uint16_t color16 = color;
+
+            return c16 | color16 << 8;
+        }
+
+        void terminal::set_color(terminal_color color)
+        {
+            _terminal_color = color;
+        }
+
+        void terminal::initilize()
+        {
+            terminal_row = 0;
             terminal_column = 0;
-        }
-        else if (data[i] == '\b')
-        {
-            terminal_column--;
-            terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
-        }
-        else
-        {
-            terminal_putchar(data[i], color);
+            _terminal_color = terminal::make_color(vga_color::white, vga_color::black);
+            terminal_buffer = (uint16_t*)0xb8000;
+
+            terminal::clear();
         }
     }
-}
-
-void mirus::terminal_clear()
-{
-    using namespace mirus;
-
-    for (int i = 0; i < 80 * 25; i++)
-    {
-        terminal_buffer[i] = 0;
-    }
-
-    // Move the hardware cursor back to the start.
-    terminal_row = 0;
-    terminal_column = 0;
-
-    terminal_move_cursor();
-}
-
-void mirus::terminal_clear(uint8_t color)
-{
-    using namespace mirus;
-
-    for (int i = 0; i < 80 * 25; i++)
-    {
-        terminal_putchar(' ', color);
-    }
-
-    // Move the hardware cursor back to the start.
-    terminal_row = 0;
-    terminal_column = 0;
-
-    terminal_move_cursor();
-}
-
-// TODO: does not scroll correctly
-void mirus::terminal_scroll()
-{
-    using namespace mirus;
-
-    uint8_t blank = make_color(COLOR_BLACK, COLOR_BLACK);
-    unsigned temp;
-    unsigned short* vidmem = nullptr;
-
-    vidmem = (unsigned short*)0xB8000;
-
-    temp = terminal_column - 25 + 1;
-
-    mirus::memcpy(vidmem, vidmem + temp * 80, (25 - temp) * 80 * 2);
-    mirus::memsetw(vidmem + (25 - temp) * 80, blank, 80);
-
-    terminal_column = 25 - 1;
-}
-
-void mirus::terminal_move_cursor()
-{
-    using namespace mirus;
-
-    unsigned temp;
-
-    /* The equation for finding the index in a linear
-    *  chunk of memory can be represented by:
-    *  Index = [(y * width) + x] */
-    temp = terminal_row * 80 + terminal_column;
-
-    /* This sends a command to indicies 14 and 15 in the
-    *  CRT Control Register of the VGA controller. These
-    *  are the high and low bytes of the index that show
-    *  where the hardware cursor is to be 'blinking'. To
-    *  learn more, you should look up some VGA specific
-    *  programming documents. A great start to graphics:
-    *  http://www.brackeen.com/home/vga */
-    mirus::outb(0x3D4, 14);
-    mirus::outb(0x3D5, temp >> 8);
-    mirus::outb(0x3D4, 15);
-    mirus::outb(0x3D5, temp);
 }
