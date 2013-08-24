8637ce05d20e80a11cf98cd34a9528d34841d30a
Joshua Beitler <joshbeitler@gmail.com>
2013-08-24 16:14:09 -0600

    Added a file to hold the configure target

--- build/lib/tasks/configure.rb
+++ build/lib/tasks/configure.rb
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# run qemu
-desc "Run Mirus in Qemu"
-task :qemu do
+# config
+desc "Configure your build"
+task :configure do
+
 end
