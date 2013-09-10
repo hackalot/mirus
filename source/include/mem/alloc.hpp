f11a0a427dd5f2fc952105bdf8a049ebd8b88c4f
Joshua Beitler <joshbeitler@gmail.com>
2013-09-10 10:22:39 -0600

    Added framework for allocator

---
+++ source/include/mem/alloc.hpp
@@ -0,0 +1,41 @@
+// Copyright 2013 Mirus Project
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+// alloc.hpp - joshbeitler's home-grown, organic stack allocator
+//			   explaination avalible on wiki
+//
+
+namespace mirus
+{
+	namespace memory
+	{
+		class stack_allocator
+		{
+		public:
+			// Install the stack allocator, perform basic
+			// initilization functions
+			void install(size_t memory_size, 
+				unsigned int* kernel_start,
+				unsigned int* kernel_end,
+				size_t block_size);
+
+			// Allocate a region of memory
+			void k_alloc(size_t size);
+
+			// Free a region of memory
+			void k_free(void* val);
+		};
+	} // !namespace
+} // !namespace
