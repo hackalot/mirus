6364148b38fba69bd5060aee12f57585f3370759
Joshua Beitler <joshbeitler@gmail.com>
2013-10-16 11:32:35 -0600

    Build plz

--- source/kernel/boot/ramdisk.cpp
+++ source/kernel/boot/ramdisk.cpp
@@ -64,9 +64,7 @@ namespace mirus
                 if (size % 512)
                     address += 512;
 
-                // ktrace(trace_level::log, "Filename: %s\n", header->filename);
-                const char* val = *((header) + 512);
-                ktrace(trace_level::log, "%s: %s\n", header->filename, val);
+                ktrace(trace_level::log, "Filename: %s\n", header->filename);
             }
 
         }
