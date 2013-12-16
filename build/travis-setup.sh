97fa8cbf4ca16b4b6860cc220c1bc6ea42a9ad5d
Joshua Beitler <joshbeitler@gmail.com>
2013-12-16 06:40:26 -0700

    Remove un-needed packages

--- build/travis-setup.sh
+++ build/travis-setup.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Set up clang development libraries
-sudo apt-get install libclang-dev
-
-# Install pip
-sudo apt-get install python-pip
-sudo pip install cldoc
-pip install pyparsing
-
 # install assembler
 sudo apt-get install yasm
 
