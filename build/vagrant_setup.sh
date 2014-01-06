33a3c325ccc31f326878b7e7c666ae5d968a21d5
Joshua Beitler <joshbeitler@gmail.com>
2014-01-06 09:33:46 -0700

    Vagrant

--- build/vagrant_setup.sh
+++ build/vagrant_setup.sh
@@ -3,10 +3,16 @@
 # The box we use for Vagrant runs Arch, so it is very bare bones.  The following
 # are some base dependencies that will need to be installed for building.
 
+# Box may be out of date
+sudo pacman -Syy
+
 # Set up our exotic toolchain choices
-sudo pacman -S yasm
-sudo pacman -S xorriso
-sudo pacman -S clang
+sudo pacman -S yasm --noconfirm
+sudo pacman -S xorriso --noconfirm
+sudo pacman -S clang --noconfirm
 
 # Go go gem rangers
 gem install colorize
+
+# Build
+rake
